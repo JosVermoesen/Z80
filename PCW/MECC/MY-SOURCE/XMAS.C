0596873928c2607a2b7d74ea0c422b2abe06e9bf
Jos Vermoesen <JosVermoesen@users.noreply.github.com>
2023-01-30 18:49:38 +0100

    fix

--- PCW/MECC/MY-SOURCE/XMAS.C
+++ PCW/MECC/MY-SOURCE/XMAS.C
@@ -1,37 +1,36 @@
 /*	XMAS.C
 
-	PCW XMAS Tree for MESCC.
-	Copyright (c) 2022 Jos Vermoesen.
+    PCW XMAS Tree for MESCC.
+    Copyright (c) 2022 Jos Vermoesen.
 
-	This program is free software; you can redistribute it and/or modify it
-	under the terms of the GNU General Public License as published by the
-	Free Software Foundation; either version 2, or (at your option) any
-	later version.
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2, or (at your option) any
+    later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
     hint:
     Use a submit batch file to compile
      cc xmas
-     ccopt xmas    
      zsm xmas
      hextocom xmas
      xmas
 */
 
 /* Some defs. to save memory
-*/
-#define CC_NO_ARGS   // To exclude ARGC & ARGV code.
+ */
+#define CC_NO_ARGS // To exclude ARGC & ARGV code.
 
 /* Standard libraries
-*/
+ */
 #include <mescc.h>
 #include <printf.h>
 
